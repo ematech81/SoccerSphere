c247d2e5ddaa1d7cf878c882cf524cb94a817ab6
ematech81 <139646183+ematech81@users.noreply.github.com>
2023-11-23 13:30:01 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+// function to open the side bar
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+// function to close the side bar
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
